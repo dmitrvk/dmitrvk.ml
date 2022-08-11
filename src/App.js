ec1b04f9ac2bb111f4df09295722f25a4ade489b
Dmitry Kalyukov <dmitrykalyukov@gmail.com>
2022-08-11 12:29:18 +0200

    Move social links to separate component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import Footer from "./components/Footer";
+import Social from "./components/Social";
 
 export default function App() {
   return (
@@ -13,20 +14,7 @@ export default function App() {
           <Link to="/test" className="a-test"><li>Test</li></Link>
         </ul>
       </nav>
-      <div class="social">
-        <a href="https://linkedin.com/in/dmitrykalyukov" target="_blank" rel="noreferrer">
-          <img class="social-icon" src="https://simpleicons.org/icons/linkedin.svg" alt="LinkedIn"/>
-        </a>
-        <a href="https://github.com/dmitrvk" target="_blank" rel="noreferrer">
-          <img class="social-icon" src="https://simpleicons.org/icons/github.svg" alt="GitHub"/>
-        </a>
-        <a href="https://instagram.com/dmitry.kalyukov" target="_blank" rel="noreferrer">
-          <img class="social-icon" src="https://simpleicons.org/icons/instagram.svg" alt="Instagram"/>
-        </a>
-        <a href="https://t.me/dmitrvk" target="_blank" rel="noreferrer">
-          <img class="social-icon" src="https://simpleicons.org/icons/telegram.svg" alt="Telegram"/>
-        </a>
-      </div>
+      <Social />
       <Footer />
     </div>
   );
