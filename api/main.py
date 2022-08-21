dcfc1c01d42cb45ffb573ffa1a986fcdb601453b
Dmitry Kalyukov <dmitrykalyukov@gmail.com>
2022-08-21 19:12:44 +0200

    Create a new app

--- api/main.py
+++ api/main.py
@@ -1,50 +1,21 @@
-import pathlib
-from typing import Final, Generator
+from typing import Final
 
-from fastapi import APIRouter, FastAPI, HTTPException, status
-from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
+import flask
 
 
-HOST: Final[str] = 'https://dmitrvk.ml'
+app = flask.Flask(__name__)
 
 
-app = FastAPI(
-    docs_url='/api/docs',
-    openapi_url='/api/openapi.json',
-)
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=(
-      'http://localhost:3000',
-      'https://dmitrvk.ml',
-    ),
-    allow_credentials=True,
-    allow_methods=('*',),
-    allow_headers=('*'),
-)
-
-router = APIRouter()
+class Endpoints:
+    ROOT: Final[str] = '/'
+    NOTES: Final[str] = '/notes'
 
 
-class Note(BaseModel):
-    title: str
-    pages: list[str]
+@app.route(Endpoints.ROOT)
+def index():
+    return f'<html><a href="{Endpoints.NOTES}">Notes</a></html>'
 
 
-@router.get('/notes/{slug}/', response_model=Note)
-async def get_note(slug: str) -> Note:
-    pages_dir: pathlib.Path = pathlib.Path(f'/public/notes/{slug}/pages/')
-    pages: Generator = (f'{HOST}{path}' for path in pages_dir.iterdir())
-
-    if pages_dir.exists():
-        return Note(
-            title=slug.capitalize(),
-            pages=sorted(pages),
-        )
-
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
-
-
-app.include_router(router, prefix='/api')
+@app.route(Endpoints.NOTES)
+def notes():
+    return '<html></html>'
---
+++ api/test_main.py
@@ -0,0 +1,25 @@
+import flask
+
+
+import main
+
+
+class TestMain:
+    def test_main_app(self) -> None:
+        assert isinstance(main.app, flask.Flask)
+
+    def test_index_page(self) -> None:
+        assert '<html>' in main.index()
+        assert f'href="{main.Endpoints.NOTES}' in main.index()
+
+    def test_routes_exist(self) -> None:
+        endpoints = (
+            value
+            for attr, value in vars(main.Endpoints).items()
+            if not attr.startswith('__') and not attr.endswith('__')
+        )
+
+        rules = main.app.url_map.iter_rules()
+
+        for route in endpoints:
+            assert route in (str(rule) for rule in rules)
